a0ae53e567530d95acc4f646a3769eb5d8674ba5
Syric <76715986+Syrikal@users.noreply.github.com>
2023-02-09 17:37:53 -0500

    Initial Commit

---
+++ src/main/java/syric/toolbooster/Toolbooster.java
@@ -0,0 +1,25 @@
+package syric.toolbooster;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("toolbooster")
+public class Toolbooster
+{
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "toolbooster";
+
+    public Toolbooster() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ToolboosterItems.register(modEventBus);
+    }
+}
---
+++ src/main/java/syric/toolbooster/ToolboosterItems.java
@@ -0,0 +1,27 @@
+package syric.toolbooster;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import se.mickelus.tetra.items.TetraItemGroup;
+
+public class ToolboosterItems {
+    // create DeferredRegister object
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Toolbooster.MODID);
+
+    static void register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
+
+    public static final RegistryObject<Item> TOOLBOOSTER = ITEMS.register("toolbooster",
+            () -> new Item(new Item.Properties().tab(TetraItemGroup.instance).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> LEGEND_TOOLBOOSTER = ITEMS.register("legend_toolbooster",
+            () -> new Item(new Item.Properties().tab(TetraItemGroup.instance).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> CREATIVE_TOOLBOOSTER = ITEMS.register("creative_toolbooster",
+            () -> new Item(new Item.Properties().tab(TetraItemGroup.instance).rarity(Rarity.EPIC)));
+
+
+}
